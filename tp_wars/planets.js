c6e88091606091bddbe1f0202feb8dfe18105545
BucKz96 <lefevre.maxime96@gmail.com>
2026-07-01 14:26:52 +0200

    add planets details

--- tp_wars/planets.js
+++ tp_wars/planets.js
@@ -1,5 +1,13 @@
 const planets_list = document.getElementById('planets-list')
 const planets_total = document.getElementById('planets-total')
+const detail_name = document.getElementById('detail-name')
+const detail_population = document.getElementById('detail-population')
+const detail_diameter = document.getElementById('detail-diameter')
+const detail_climate = document.getElementById('detail-climate');
+const detail_gravity = document.getElementById('detail-gravity')
+const detail_terrain = document.getElementById('detail-terrain')
+
+
 const planets_url = "https://swapi.dev/api/planets/";
 
 const getAllPlanets = async (planets_url) => {
@@ -34,6 +42,7 @@ const getPlanetsList = (data) => {
         <td>${planet.terrain}</td>
         <td>${planet.population}</td>
         `;
+        row.addEventListener('click', () => getPlanetDetails(planet.name, data));
         planets_list.append(row);
     });
 };
@@ -42,6 +51,21 @@ const getPlanetsData = async () => {
     const data = await getAllPlanets(planets_url);
     getPlanetsCount(data);
     getPlanetsList(data);
+};
+
+const getPlanetDetails = (planet_name, data) => {
+  console.log(planet_name);
+  const planet_details = data.results.find(planet => planet.name === planet_name);
+  displayPlanetDetails(planet_details);
+};
+
+const displayPlanetDetails = (planet_details) => {
+  detail_name.textContent = planet_details.name;
+  detail_population.textContent = planet_details.population;
+  detail_diameter.textContent = planet_details.diameter;
+  detail_climate.textContent = planet_details.climate;
+  detail_gravity.textContent = planet_details.gravity;
+  detail_terrain.textContent = planet_details.terrain;
 };
 
 getPlanetsData();
